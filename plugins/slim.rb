82e57d39688ae3f190b9acb5b680a97fea656be6
Michael Chang <m9chang@uwaterloo.ca>
2012-01-12 14:49:49 +0000

    Initial attempt at adding slim support.

---
+++ plugins/slim.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  require 'slim'
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /slim/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      begin
+        engine = Slim::Engine.new(content)
+        engine.render
+      rescue StandardError => e
+          puts "!!! SLIM Error: " + e.message
+      end
+    end
+  end
+end
